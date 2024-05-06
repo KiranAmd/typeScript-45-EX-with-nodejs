8217c0344feeedcf1aa67b997872c87b78bbcfc3
kiranAmd <159790884+KiranAmd@users.noreply.github.com>
2024-05-05 22:03:04 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,52 @@
+//Making  a variables
+
+let Five = 5;
+
+let Ten = 10;
+
+//TEST 1
+console.log("Test 1: five is equal to 5?");
+console.log(five == 5);
+
+//TEST 2
+console.log("\nTest 2: ten is equal to 10?");
+console.log(ten == 10);
+
+//TEST 3
+console.log("\nTest 3: 5 in not equal to 10");
+console.log(five != ten);
+
+//TEST 4
+console.log("\nTest 4: 10 is greater than 5");
+console.log(ten > 5);
+
+//TEST 5
+console.log("\nTest 5: 5 is smaller than 10");
+console.log(five < ten);
+
+//TEST 6
+console.log("\nTest 6: 10 is smaller than 5");
+console.log(ten < 5);
+
+//TEST 7
+console.log("\nTest 7: 5 is  equal to 10");
+console.log(five == ten );
+
+//TEST 8
+console.log("\nTest 8: 10 is not equal to 10");
+console.log(ten != ten);
+
+//TEST 9
+console.log("\nTest 9: 5 is greater than 10");
+console.log(five > ten);
+
+//TEST 10
+console.log("\nTest 10: 100 is smaller than 50");
+console.log(100 < 50);
+
+
+
+
+
+
+
