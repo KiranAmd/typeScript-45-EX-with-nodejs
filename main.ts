966117a76522d931c560cef881b90e0969d8f023
kiranAmd <159790884+KiranAmd@users.noreply.github.com>
2024-05-08 10:11:22 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,40 @@
+//define variable
+let alienColor = "green";
+
+//Using If and Else-if statements
+if(alienColor === "green"){
+    console.log("Version 1) you shot down green alien you have earned 5 points");
+}
+else if(alienColor === "yellow"){
+    console.log("Version 2 ) you shot down yellow alien you have earned 10 points");
+}
+else if(alienColor === "red"){
+    console.log("you shot down red alien you have earned 15 points");
+}
+
+//version 2
+
+let alienColor2 = "yellow";
+
+if(alienColor2 === "green"){
+console.log("you shot down green alien you have earned 5 points");
+}
+else if(alienColor2 === "yellow"){
+    console.log("\nVersion 2) you shot down yellow alien you earned 10 points");    
+}
+else if(alienColor2 === "red"){
+    console.log("you shot down red alien you have earned 15 points");
+}
+
+// Version 3
+let alienColor3 = "red";
+
+if(alienColor3 === "green"){
+  console.log("you shot down green alien you have earned 5 points");
+}
+else if (alienColor3 === "yellow"){
+    console.log("you shot down yellow alien you have earned 10 points");
+}
+else if(alienColor3 === "red"){
+    console.log("\nVersion 3) you shot red alien you have earned 15 points");
+}
