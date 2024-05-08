7167bb9c858a880ad8e2a83d852b58266287148d
kiranAmd <159790884+KiranAmd@users.noreply.github.com>
2024-05-08 10:09:13 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,19 @@
+// Define variable
+let alienColor = "green"
+
+// First version
+if (alienColor === "green"){
+    console.log("player just earned 5 points for shooting the alien.");
+}
+else{
+    console.log("player just earned 10 points.");
+}
+
+//second version
+
+if (alienColor === "blue"){
+    console.log("I am comr from If statment.");
+}
+else{
+    console.log("I am come from Else statment.");
+}
