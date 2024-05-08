38e77031c42c011719a7ea8c7a9ab73f8a0d67d9
kiranAmd <159790884+KiranAmd@users.noreply.github.com>
2024-05-08 10:16:01 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,22 @@
+// Creating a Array
+let favorite_fruits = ["watermelon", "grapes","mangoes"]
+
+
+// Using 5 Independent IF Statments
+
+if (favorite_fruits.includes("mangoes")){
+    console.log("I Really Like mangoes");
+}
+if (favorite_fruits.includes("grapes")){
+    console.log("I Really Like grapes");
+}
+if (favorite_fruits.includes("cherry")){
+    console.log("I Really like cherry");
+}
+if (favorite_fruits.includes("watermelon")){
+    console.log("I Really like watermelon");
+}
+if(favorite_fruits.includes("orange")){
+    console.log("I Really like orange");
+    
+}
