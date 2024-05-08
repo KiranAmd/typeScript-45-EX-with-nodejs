c629149f629a472fde94ca976008d9777dfb644f
kiranAmd <159790884+KiranAmd@users.noreply.github.com>
2024-05-08 10:51:13 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,11 @@
+//Creating a Array 
+let petAnimals = ["Cat","Dog","Rabit"]
+
+// Using for-Loop
+for (let onePet of petAnimals){
+console.log(`A ${onePet} would make a agreat pet.`);
+}
+
+//Print a Message outside of for-loop
+
+console.log("any of these animals would make a great pet!");
