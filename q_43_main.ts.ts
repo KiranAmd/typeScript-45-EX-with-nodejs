fa8948662e7c7ba60826ed7fa8ac722f561d83ee
kiranAmd <159790884+KiranAmd@users.noreply.github.com>
2024-05-08 11:08:35 -0700

    Add files via upload

---
+++ q_43_main.ts.ts
@@ -0,0 +1,35 @@
+//Define the function to show magicians names
+
+function show_magicians(magicians: string[]){
+    magicians.forEach(name => console.log(name));
+}
+
+//function to make magicians great through .map() it will modify array
+function make_great(magicians: string[]){
+  return magicians.map(name => `The Great ${name}`);
+
+}
+
+
+
+//Define an array of magicians names
+let magician_names =["Harry poter","Kiran","Ahmed"]
+
+//Making a copy of orignal array through .slice() function
+
+let copy_magician_names = magician_names.slice()
+
+//Modify the copied rray to include "THE GREAT" with their names
+let  copy_great_magician = make_great(copy_magician_names);
+
+//Show both arrays Orignal and copied
+
+//ORIGNAL
+console.log("Orignal Array\n");
+
+show_magicians(magician_names);
+
+//COPIED
+console.log("\nCopied Array\n");
+
+show_magicians(copy_great_magician);
