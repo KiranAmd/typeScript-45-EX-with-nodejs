e053b9eb3b5831edb42cd550182123e50f8dc8dd
kiranAmd <159790884+KiranAmd@users.noreply.github.com>
2024-05-13 11:56:57 -0700

    Add files via upload

---
+++ q_21_main.ts
@@ -0,0 +1,13 @@
+interface NewAddmision {
+    ClassesName: string;
+    location: string;
+    TotalStudents: number;
+}
+
+let NewAddmision ={
+    ClassesName : "Fs-one and Fs-two",
+    location : " gulshan camp",
+    TotalStudents : 100 
+};
+
+console.log(NewAddmision);
