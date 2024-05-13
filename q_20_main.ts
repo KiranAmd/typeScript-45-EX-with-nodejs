261758d8e2199eea05849494661b5e33166779e8
kiranAmd <159790884+KiranAmd@users.noreply.github.com>
2024-05-13 11:57:59 -0700

    Add files via upload

---
+++ q_20_main.ts
@@ -0,0 +1,8 @@
+// Making aProgramming Languages Array
+let programmingLanguage: string[] = ["Typescript", "javascript","Python","PHP"];
+
+// Print the message of list
+console.log("list of Programming Language:");
+
+// Print thevalues of array in the form of list
+programmingLanguage.forEach(language => console.log(language));
