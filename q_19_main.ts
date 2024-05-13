086926088542f34286e91b4315cfcf93a9b2dfee
kiranAmd <159790884+KiranAmd@users.noreply.github.com>
2024-05-13 11:58:30 -0700

    Add files via upload

---
+++ q_19_main.ts
@@ -0,0 +1,7 @@
+let guestList = ["khadija", "ramsha" ,"farah", "kiran"];
+
+//guestList.forEach(oneguest => console.log(`salam ${oneguest}, would you like to dinner with me?`));
+
+let lenghtGuest: number = guestList.length;
+
+console.log(`We are Inviting total ${lenghtGuest} guest.`);
